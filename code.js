1bdfb19719f6ff4f43dc2a8b8fd1a9c460bb8c7a
matthewpetralia <petraliamatthew@gmail.com>
2022-04-14 21:50:26 +1000

    Fixed head tag

--- code.js
+++ code.js
@@ -1,15 +1,10 @@
 /*eslint-env es6*/
 /*global require*/
 
-//document.getElementsByTagName("head")[0].innerHTML += `
-//    <meta name="viewport" content="width=device-width, initial-scale=1">
-//    <link rel="stylesheet" href="normalize.css">
-//    <link rel="preconnect" href="https://fonts.googleapis.com">
-//    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-//    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@700&family=Hubballi&family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
-//    <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined" rel="stylesheet">
-//    <link rel="stylesheet" href="content.css">
-//    <link rel="stylesheet" href="main.css">`;
+document.getElementsByTagName("head")[0].innerHTML += `
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@700&family=Hubballi&family=Lato:wght@300;400;700&display=swap" rel="stylesheet">`;
 
 function writeHTML() {
     let B1;
