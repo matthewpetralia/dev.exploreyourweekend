48ca68b11cde7200b54eaf221d53fadcd124f65c
matthewpetralia <petraliamatthew@gmail.com>
2022-05-29 19:26:06 +1000

    PageSpeed Testing

--- code.js
+++ code.js
@@ -4,11 +4,11 @@
 document.getElementsByTagName("head")[0].innerHTML += `
     <link rel="icon" type="image/x-icon" href="/SVG/Explore%20your%20Weekend%20Logo.svg">
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined" rel="stylesheet">
-    <link href="https://use.typekit.net/ade3twf.css" rel="stylesheet">
 `;
 
 
 function writeHTML() {
+
     let main = document.querySelector(".Main");
 
     let writeContent;
@@ -38,7 +38,7 @@ function writeHTML() {
 
 
         main.children[e].innerHTML =
-            '<picture><source srcset="../Images/m/m-' + A1[0] + '.webp" media="(orientation: portrait)"><img src="../Images/' + A1[0] + '.webp" onerror="this.onerror=null;this.src=' + "'" + '../Images/m/m-Sp_.webp' + "'" + ';" alt="' + A1[1] + ' ' + A1[4] + ' - Explore your Weekend"></picture>' +
+            '<picture><source srcset="../Images/m/m-' + A1[0] + '.webp" media="(orientation: portrait)"><img src="../Images/' + A1[0] + '.webp" onerror="this.onerror=null;this.src=' + "'" + '../Images/m/m-Sp_.webp' + "'" + ';" alt="' + A1[1] + ', ' + A1[4] + ' - Explore your Weekend"></picture>' +
             '<a class="bwd"></a><a class="fwd"></a><div class="InfoPanel"><h4>' + A1[4] + '</h4><h3>' + A1[1] + '</h3><div class="tagContent">' + A1[2] + '</div><p>' + A1[3] + '</p></div>';
 
 
@@ -50,6 +50,14 @@ function writeHTML() {
     document.querySelector(".Nav .tags").innerHTML = '<a href="../"><img src="/SVG/Explore%20your%20Weekend%20Logo.svg" alt="Explore your Weekend"></a>' + text;
 
     document.getElementsByTagName("title")[0].innerHTML += " | Explore your Weekend";
+
+    document.getElementsByTagName("head")[0].innerHTML +=
+        '<link href="https://use.typekit.net/ade3twf.css" rel="stylesheet">'
+
+    let nextLast = document.querySelector(".Main").lastElementChild;
+    nextLast.querySelector(".fwd").href = "#" + (document.querySelectorAll(".Main > div")[0]).id;
+
+    document.querySelectorAll(".Main > div .bwd")[0].href = "#" + document.querySelectorAll(".Main > div")[(document.querySelectorAll(".Main > div").length) - 1].id;
 
 
     for (let j = 0; j < (document.querySelectorAll(".Main > div")).length; j++) {
@@ -66,6 +74,8 @@ function writeHTML() {
 
         (document.getElementsByClassName('bwd')[j + 1]).href = "#" + (document.querySelectorAll(".Main > div")[j]).id;
     }
+
+
 }
 
 function myFunction() {
@@ -79,12 +89,4 @@ function myFunction() {
         document.querySelector(".Nav > a").style.borderRadius = "0px";
         document.getElementById("expand").innerHTML = "expand_more";
     }
-}
-
-function arrows() {
-    let nextLast = document.querySelector(".Main").lastElementChild;
-    nextLast.querySelector(".fwd").href = "#" + (document.querySelectorAll(".Main > div")[0]).id;
-
-
-    document.querySelectorAll(".Main > div .bwd")[0].href = "#" + document.querySelectorAll(".Main > div")[(document.querySelectorAll(".Main > div").length) - 1].id;
 }
