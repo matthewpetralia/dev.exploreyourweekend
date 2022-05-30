2fa6b814329f7d5e32eea0cbd38a82bdc45856e3
matthewpetralia <petraliamatthew@gmail.com>
2022-05-30 20:02:04 +1000

    Reworked JS Reworked Index Removed Charset

--- code.js
+++ code.js
@@ -11,8 +11,6 @@ function writeHTML() {
 
     let main = document.querySelector(".Main");
 
-    let writeContent;
-
     let createNav = `
         <div class="Nav">
         <a href="javascript:void(0);" onclick="myFunction()">
@@ -33,9 +31,6 @@ function writeHTML() {
 
 
         const A1 = main.children[e].innerHTML.split(";");
-        let ref = "A";
-
-
 
         main.children[e].innerHTML =
             '<picture><source srcset="../Images/m/m-' + A1[0] + '.webp" media="(orientation: portrait)"><img src="../Images/' + A1[0] + '.webp" onerror="this.onerror=null;this.src=' + "'" + '../Images/m/m-Sp_.webp' + "'" + ';" alt="' + A1[1] + ', ' + A1[4] + ' - Explore your Weekend"></picture>' +
@@ -73,6 +68,65 @@ function writeHTML() {
         (document.getElementsByClassName('fwd')[j]).href = "#" + (document.querySelectorAll(".Main > div")[j + 1]).id;
 
         (document.getElementsByClassName('bwd')[j + 1]).href = "#" + (document.querySelectorAll(".Main > div")[j]).id;
+    }
+
+
+}
+
+function writeIndex() {
+
+    let main = document.querySelector(".Landing");
+
+    let createNav = `
+        <div class="Nav">
+        <a href="javascript:void(0);" onclick="myFunction()">
+            <span class="material-icons-outlined" id="expand">expand_less</span>
+        </a>
+        <div id="myLinks">
+            <div class="InfoPanel">
+                <div class="tags">
+                </div>
+            </div>
+        </div>
+    </div>`
+    main.insertAdjacentHTML('afterend', createNav)
+
+    let text = " ";
+
+    for (e = 0; e < main.children.length; e++) {
+
+
+        const A1 = main.children[e].innerHTML.split(";");
+
+        main.children[e].innerHTML =
+            '<picture><source srcset="../Images/m/m-' + A1[0] + '.webp" media="(orientation: portrait)"><img src="../Images/' + A1[0] + '.webp" onerror="this.onerror=null;this.src=' + "'" + '../Images/m/m-Sp_.webp' + "'" + ';" alt="' + A1[1] + ' ' + A1[2] + ' - Explore your Weekend"></picture>' +
+            '<div class="InfoPanel"><h3>' + A1[1] + '</h3><h4>' + A1[2] + '</h4><div class="tagContent">' + A1[3] + '</div></div>';
+
+
+        text += document.querySelector("#myLinks .tags").innerHTML = "<a href='#" + (A1[1].replace(/\s+/g, '')) + (A1[2].replace(/\s+/g, '')) + "' onclick='myFunction()'>" + A1[1] + " " + A1[2] + "</a>";
+
+
+    }
+
+    document.querySelector(".Nav .tags").innerHTML = '<a href="../"><img src="/SVG/Explore%20your%20Weekend%20Logo.svg" alt="Explore your Weekend"></a>' + text;
+
+    document.getElementsByTagName("title")[0].innerHTML += " | Explore your Weekend";
+
+    document.getElementsByTagName("head")[0].innerHTML +=
+        '<link href="https://use.typekit.net/ade3twf.css" rel="stylesheet">'
+
+
+
+
+    for (let j = 0; j < (document.querySelectorAll(".Landing > div")).length; j++) {
+        var allTheData =
+            document.getElementsByClassName('tagContent')[j].textContent.split(',');
+        var separateList = '<div class="tags">';
+        allTheData.forEach(function (value) {
+            separateList += '<div>' + value + '</div>';
+        });
+        separateList += '</div>';
+        document.getElementsByClassName("tagContent")[j].innerHTML = separateList;
     }
 
 
